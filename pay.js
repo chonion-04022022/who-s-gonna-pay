26fc34f7afc45f3cf1abaffc7c0edee91eaa1ddb
Fahim Morshed Nion <114305756+chonion-04022022@users.noreply.github.com>
2024-01-01 15:55:53 +0600

    Create pay.js

---
+++ pay.js
@@ -0,0 +1,8 @@
+var names=["ali","jamali","joshim","kuddus","mobarok","jim","pam","dwight"];
+function whospaying(names){
+    console.log(names[Math.floor((Math.random()*names.length))] + " gonna pay the bill");
+    console.log(Math.random());
+    
+}
+
+whospaying(names);
